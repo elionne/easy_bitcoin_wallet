e78d95e101ef73eaeafa38504d1464aad84a813a
Elionne <elionne@yieu.eu>
2014-08-07 13:35:00 +0200

    benchs: add list_vowels to testbench

--- testbench.vhdl
+++ testbench.vhdl
@@ -10,7 +10,11 @@ architecture testbench_arch of testbench is
 
     component testbench_pw_string
     end component;
+
+    component testbench_vowels
+    end component;
 begin
   stack : testbench_recursive_stack;
   final_string : testbench_pw_string;
+  list_vowels : testbench_vowels;
 end testbench_arch;
